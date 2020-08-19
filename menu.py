9c3bcd04ba32cf88fd4257f3aa36763a550c8203
palomara <palomarangelll@hotmail.com>
2020-08-18 23:05:23 -0300

    fixing code + course options

--- menu.py
+++ menu.py
@@ -1,10 +1,19 @@
 nome = []
-cpf = []
+cpf =[]
 estado = []
 curso = []
 aluno = [nome, cpf, estado, curso]
+codigo = ["A01", "A02", "A03","A04"]
+todos = ["AC","AL","AP", "AM","BA","CE","DF","ES","GO","MA","MT","MS", "MG","PA","PB","PR","PE","PI","RJ", "RN","RS","RO","RR","SC",'SP',"SE",'TO']
+nordeste = ["AL","BA","CE","MA","PB","PE","PI","RN","SE"]
+centroOeste = ["DF","GO","MT","MS","RR"]
+nss = ["RR","AM","AC","RO","PA","AP","TO","SP","MG","ES","RJ","PR","SC","RS"]
+sudeste = ["SP","MG","ES"]
+estados = [todos, nordeste, centroOeste,nss, sudeste]
+nomeCurso = ["Al e Machine Learning", "Business Inteligence", "Governança em TI","Programação Python", "Programação JAVA"]
+cursos = [codigo, nomeCurso, estados ]
 sair = 0
-while (sair != 4):
+while(sair != 4):
     menu = int(input("1. Fazer Inscrição\n2. Alterar Inscrição\n3. Listar Inscrições\n4. Sair\n:"))
     if menu == 1:
         print("Para realizar a inscrição informe os seguintes dados: ")
@@ -16,14 +25,11 @@ while (sair != 4):
         cpf.append(_cpf)
         estado.append(_estado)
         curso.append(_curso)
-        print(aluno)
     elif menu == 2:
         opcao1 = int(input("1. Alterar inscrição pelo cpf\n2. Alterar inscrição pelo código de inscrição\n:"))
         if opcao1 == 1:
             alteraInscricaoCpf = int(input("Informe seu CPF: "))
-            cpf.index(alteraInscricaoCpf)
-
-        elif opcao1 == 2:
+        elif opcao1 ==  2:
             alteraInscricaoCD = int(input("Informe o código de inscrição: "))
         else:
             print("Opção Inválida")
